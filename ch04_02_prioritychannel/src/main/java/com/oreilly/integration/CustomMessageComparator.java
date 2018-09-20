a99c4a98c76a26bd59c1ed51dedfc050fed95d44
Karimov R <rkarimov.r@gmail.com>
2018-09-20 18:32:59 +0400

    Add priority channel example

---
+++ ch04_02_prioritychannel/src/main/java/com/oreilly/integration/CustomMessageComparator.java
@@ -0,0 +1,27 @@
+package com.oreilly.integration;
+
+import org.springframework.messaging.Message;
+
+import java.util.Comparator;
+
+public class CustomMessageComparator implements Comparator<Message<String>> {
+
+	@Override
+	public int compare(Message<String> message, Message<String> message2) {
+
+		String payload = message.getPayload();
+		String payload2 = message2.getPayload();
+		boolean isPayloadEven = Integer
+                .valueOf(payload.substring(payload.length() - 1)) % 2 == 0;
+        boolean isPayload2Even = Integer
+                .valueOf(payload2.substring(payload2.length() - 1)) % 2 == 0;
+
+        if ((isPayloadEven && isPayload2Even) || (!isPayloadEven && !isPayload2Even)) {
+            return 0;
+        } else if (isPayloadEven) {
+            return -1;
+        } else {
+            return 1;
+        }
+	}
+}
