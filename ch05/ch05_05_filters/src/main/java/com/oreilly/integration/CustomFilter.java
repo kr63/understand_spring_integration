945941ef5c6aed1266599f046dc991dd809693ae
Karimov R <rkarimov.r@gmail.com>
2018-09-24 14:24:09 +0400

    Add custom filter implementation

---
+++ ch05/ch05_05_filters/src/main/java/com/oreilly/integration/CustomFilter.java
@@ -0,0 +1,12 @@
+package com.oreilly.integration;
+
+import org.springframework.integration.core.MessageSelector;
+import org.springframework.messaging.Message;
+
+public class CustomFilter implements MessageSelector {
+
+    @Override
+    public boolean accept(Message<?> message) {
+        return message.getPayload().equals(7);
+    }
+}
