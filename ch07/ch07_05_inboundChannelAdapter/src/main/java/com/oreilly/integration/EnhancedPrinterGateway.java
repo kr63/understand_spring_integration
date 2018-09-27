669eaee859f107336beeb288b1773a9e7bf7427e
Karimov R <rkarimov.r@gmail.com>
2018-09-27 20:14:33 +0400

    Add inbound channel adapter as entry point of external service to the messaging system

---
+++ ch07/ch07_05_inboundChannelAdapter/src/main/java/com/oreilly/integration/EnhancedPrinterGateway.java
@@ -0,0 +1,11 @@
+package com.oreilly.integration;
+
+import org.springframework.util.concurrent.ListenableFuture;
+
+public interface EnhancedPrinterGateway {
+
+	void print(Person person);
+	
+	ListenableFuture<String> uppercase(Person person);
+	
+}
