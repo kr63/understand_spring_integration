06c08d223375496ba7d256f703d9e42c9796df8c
Karimov R <rkarimov.r@gmail.com>
2018-10-03 21:58:14 +0400

    Implement outbound channel adapter for writing files to the remote FTP server

---
+++ ch09/ch09_02_outboundChannelAdapter/src/main/java/com/oreilly/integration/FileWriterGateway.java
@@ -0,0 +1,11 @@
+package com.oreilly.integration;
+
+
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+
+public interface FileWriterGateway {
+
+    void write(@Header("fileName") String fileName,
+               @Payload String message);
+}
---
+++ ch09/ch09_02_outboundChannelAdapter/src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
@@ -0,0 +1,31 @@
+package com.oreilly.integration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@Configuration
+@ImportResource("integration-context.xml")
+public class SpringIntegrationApplication implements ApplicationRunner {
+
+    @Autowired
+    private FileWriterGateway gateway;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments arg0) {
+
+	    this.gateway.write(
+	            "example.txt",
+                "This is a test of FTP outbound channel adapter");
+	}
+
+}
