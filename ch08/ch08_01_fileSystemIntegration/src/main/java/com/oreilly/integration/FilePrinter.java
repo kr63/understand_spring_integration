dcacc44e9bc26873f4747b8b3cde44341ee309ee
Karimov R <rkarimov.r@gmail.com>
2018-10-03 11:44:12 +0400

    Implement simple file reading channel

---
+++ ch08/ch08_01_fileSystemIntegration/src/main/java/com/oreilly/integration/FilePrinter.java
@@ -0,0 +1,33 @@
+package com.oreilly.integration;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+
+public class FilePrinter {
+
+	public void print(File file){
+		
+		BufferedReader reader = null;
+		
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			
+			String line;
+			
+			while ((line = reader.readLine()) != null){
+				System.out.println(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			try {
+				Objects.requireNonNull(reader).close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
