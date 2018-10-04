fa044b7b7a867a5ad78ebbbd1f5b06d9e5040353
Karimov R <rkarimov.r@gmail.com>
2018-10-04 17:44:27 +0400

    Implement inbound jdbc channel adapter

---
+++ ch10/ch10_01_inboundChannelAdapter/src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
@@ -0,0 +1,25 @@
+package com.oreilly.integration;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@Configuration
+@ImportResource("integration-context.xml")
+public class SpringIntegrationApplication implements ApplicationRunner {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments arg0) {
+
+    }
+
+}
