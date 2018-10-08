f32f9cee081cdf619aaee9c63a8a908fe3d33cc1
Karimov R <rkarimov.r@gmail.com>
2018-10-08 22:18:28 +0400

    Implement message driven channel adapter

---
+++ ch11/ch11_03_messageDrivenChannelAdapter/src/main/java/com/oreilly/integration/SpringIntegrationApplication.java
@@ -0,0 +1,30 @@
+package com.oreilly.integration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@Configuration
+@ImportResource("integration-context.xml")
+public class SpringIntegrationApplication implements ApplicationRunner {
+
+    @Autowired
+    private PersonGateway gateway;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments arg0) {
+	    Person person = new Person(4, "Jane", "Doe");
+	    this.gateway.save(person);
+        System.out.println("The message has been sent to jms.");
+    }
+
+}
